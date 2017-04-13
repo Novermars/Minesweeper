2bd8dc2141e9d84beb38918a7b513091f1eb4bcf
Novermars <nigelovermars@outlook.com>
2017-04-13 12:48:05 +0200

    Implement feedback.

--- minesweeper/minesweeper.h
+++ minesweeper/minesweeper.h
@@ -4,18 +4,23 @@
 #include <cstddef>
 #include <vector>
 #include <iosfwd>
-#include <utility> 
 
 #include "../minesweepercell/minesweepercell.h"
 
 class Minesweeper
 {
-    std::size_t d_size;        // square only so far
+    std::size_t d_size = 9;        // square only so far
+    std::size_t d_numBombs = 10;
     std::vector<std::vector<MinesweeperCell>> d_gameBoard;
-    std::vector<std::pair<int, int>> d_bombLocations;
+    std::vector<MinesweeperCell *> d_bombLocations;
+    
     public:
-        Minesweeper(std::size_t const size = 9);
+        Minesweeper();
+        Minesweeper(std::size_t const size, std::size_t const numBoms);
+        Minesweeper(Minesweeper const &minesweeper) = delete;
+        Minesweeper &operator=(Minesweeper const &minesweeper) = delete;
         void play();
+    
     private:
         void initializeBoard();     // Places randomly d_size + 1 bombs on board
         void setNumBombsNear();
